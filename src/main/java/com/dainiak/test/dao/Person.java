af22772ab139ebf916fc233ef4cf8474d1dc5fdd
GasDa <270309Ab>
2016-02-28 11:05:55 +0300

    yes

--- src/main/java/com/dainiak/test/dao/Person.java
+++ src/main/java/com/dainiak/test/dao/Person.java
@@ -79,6 +79,6 @@ public class Person implements Serializable {
 
     @Override
     public String toString(){
-        return "id="+id+", name="+name+", age="+age+ ", isAdmin="+isAdmin+", createDate="+createDate;
+        return "id="+id+", name="+name+", age="+age+ ", isAdmin="+isAdmin+", createDate="+createDate+"yes!";
     }
 }
